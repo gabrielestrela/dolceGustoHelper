78460748aacf94faf7253dedf08ee6b1bfcfbbee
Gabriel Estrela <gabrielestrela92@gmail.com>
2018-01-05 11:01:53 -0200

    Removed unnecessary commented code

--- app/src/main/java/br/com/nespressohelper/daitan/nespressohelper/BitmapHandler.java
+++ app/src/main/java/br/com/nespressohelper/daitan/nespressohelper/BitmapHandler.java
@@ -19,22 +19,7 @@ public class BitmapHandler {
     private Bitmap bitmap;
     private ByteArrayOutputStream stream;
     private byte[] bitmapData;
-
-//    public byte[] getImageBitmapData(Bitmap bitmap) {
-//        if(bitmap == null) {
-//            drawable = ContextCompat.getDrawable(App.getContext(), R.drawable.coffecapsule);
-//            bitmap = ((BitmapDrawable) drawable).getBitmap();
-//            stream = new ByteArrayOutputStream();
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-//            bitmapData = stream.toByteArray();
-//            return bitmapData;
-//        }else{
-//            stream = new ByteArrayOutputStream();
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-//            bitmapData = stream.toByteArray();
-//            return bitmapData;
-//        }
-//    }
+    
 
     public byte[] getImageBitmapData(int index, int length, TypedArray drawables, Bitmap bitmap) {
 
@@ -59,21 +44,6 @@ public class BitmapHandler {
                 return bitmapData = stream.toByteArray();
             }
         }
-
-//        if(index >= length) {
-//            drawable = ContextCompat.getDrawable(App.getContext(), R.drawable.coffecapsule);
-//            bitmap = ((BitmapDrawable) drawable).getBitmap();
-//            stream = new ByteArrayOutputStream();
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-//            bitmapData = stream.toByteArray();
-//            return bitmapData;
-//        }else{
-//            drawable = ContextCompat.getDrawable(App.getContext(), drawables.getResourceId(index, -1));
-//            bitmap = ((BitmapDrawable) drawable).getBitmap();
-//            stream = new ByteArrayOutputStream();
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-//            return bitmapData = stream.toByteArray();
-//        }
     }
 
 }
